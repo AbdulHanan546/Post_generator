26c2fbdf6f562819c7eb52ea184a690d1ae15cae
Abdul <abdlhanan987@gmail.com>
2025-09-03 15:27:28 +0500

    update axios.js

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,   // set in Vercel dashboard
-  withCredentials: true,
-});
-
+const baseURL = process.env.REACT_APP_API_URL || "https://post-generator-2p7o.onrender.com";
+const instance = axios.create({ baseURL, withCredentials: true });
 export default instance;
+
+
